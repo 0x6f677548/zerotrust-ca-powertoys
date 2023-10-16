d59968dc1dd532f581cd9c63a950626bcf222c6a
0x6f677548 <64972114+0x6f677548@users.noreply.github.com>
2023-10-16 18:40:26 +0100

    fix: token was not being requested when not provided

--- src/entraid_tools/helpers/click.py
+++ src/entraid_tools/helpers/click.py
@@ -6,14 +6,14 @@ def get_from_ctx_if_none(
     ctx: click.Context,
     ctx_key: str,
     value: str | None = None,
-    invoke_func: Callable = None,
+    invoke_func: Callable | None = None,
 ) -> str:
     """Get a value from the context if it is None,
     otherwise invoke a function to get the value."""
     ctx.ensure_object(dict)
-    if value is not None:
+    if value:
         return value
-    elif ctx_key in ctx.obj:
+    elif ctx_key in ctx.obj and ctx.obj[ctx_key]:
         return ctx.obj[ctx_key]
     else:
         result = ctx.invoke(invoke_func)
