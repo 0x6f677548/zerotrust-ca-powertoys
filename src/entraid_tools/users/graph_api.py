a327062eecefee25ce5e95392c6d6c91ac19f508
0x6f677548 <64972114+0x6f677548@users.noreply.github.com>
2023-10-19 15:55:01 +0100

    feat: users entity

---
+++ src/entraid_tools/users/graph_api.py
@@ -0,0 +1,22 @@
+from ..helpers.graph_api import APIResponse, EntityAPI
+
+
+class UsersAPI(EntityAPI):
+    def _get_entity_path(self) -> str:
+        return "users"
+
+    def get_by_id(self, entity_id: str) -> APIResponse:
+        """Returns a user by their ID which can be the id attribute
+        or the userPrincipalName attribute
+        """
+
+        assert entity_id, "entity_id cannot be None"
+
+        # encode the hash symbol in the entity_id
+        entity_id = entity_id.replace("#", "%23")
+
+        if entity_id.startswith("$"):
+            url = f"{self.entity_url}({entity_id})"
+        else:
+            url = f"{self.entity_url}/{entity_id}"
+        return self._request_get(url)
